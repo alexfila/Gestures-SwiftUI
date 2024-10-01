fce36a9b7903597ec8b95419f184fac7b5d3254e
Alex Fila <fialex66@gmail.com>
2024-10-01 23:42:07 +0200

    Initial Commit

---
+++ Gestures-SwiftUI/Gestures_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Gestures_SwiftUIApp.swift
+//  Gestures-SwiftUI
+//
+//  Created by Alex Fila on 01/10/24.
+//
+
+import SwiftUI
+
+@main
+struct Gestures_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
